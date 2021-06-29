12730a891cc1780264346cf9545c4628fd5e726c
C+- <hulk_228@inbox.ru>
2021-06-29 15:47:43 +0400

    Class

---
+++ black_mamba/black_mamba/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace black_mamba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 3;
+            p1.y = 4;
+            p1.sym = '*';
+            p1.Draw();
+            Console.ReadKey();
+        }
+    }
+}
